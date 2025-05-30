47fdad2480a6ac221bb59add8c5269c3c21de028
fnhnafiz <nafsinsifan2k21@gmail.com>
2025-05-30 16:48:10 +0600

    Problem 1 was if  boolen will false the the input string value will be lowerscas and problem 2 and problem 3 solve the all about of requirments

---
+++ src/server.ts
@@ -0,0 +1,128 @@
+// Problem 1:
+// Description: Create a function that takes a string and an optional boolean.
+
+// If the boolean is true or not provided, return the string in uppercase.
+// If the boolean is false, return the string in lowercase.
+// Function Signature:
+
+// function formatString(input: string, toUpper?: boolean): string
+// Example:
+
+// formatString("Hello");          // Output: "HELLO"
+// formatString("Hello", true);   // Output: "HELLO"
+// formatString("Hello", false);  // Output: "hello"
+
+function formatString(input:string,toUpper?:boolean):string{
+   if(toUpper === false){
+    return input.toLowerCase()
+   }else{
+    return input.toUpperCase()
+   }
+}
+
+// // If the boolean is true or not provided, return the string in uppercase.
+// const result = formatString("Hello");
+// console.log(result)          
+// const result2 = formatString("Hello", true);   
+// console.log(result2) 
+
+// // If the boolean is false, return the string in lowercase.
+// const result3 = formatString("Hello", false);
+// console.log(result3) 
+
+// ===============PROBLEM====2=================
+// Problem 2:
+// Description: Create a function that filters an array of objects by the rating property, returning only items with a rating of 4 or more.
+
+// Function Signature:
+
+// function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[]
+// Example:
+
+// const books = [
+//   { title: "Book A", rating: 4.5 },
+//   { title: "Book B", rating: 3.2 },
+//   { title: "Book C", rating: 5.0 }
+// ];
+
+// filterByRating(books); 
+// // Output: [ { title: "Book A", rating: 4.5 }, { title: "Book C", rating: 5.0 } ]
+const items = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 }
+];
+
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[]{
+    // const newItems = items.filter((item)=> item >= 4{})
+   return items.filter((item)=>{
+      return item.rating >= 4 
+    })
+}
+
+// const result = filterByRating(items)
+// console.log(result)
+
+
+// ===================PROBLEM 3=================
+// Problem 3:
+// Description: Create a generic function that concatenates multiple arrays of the same type using rest parameters.
+
+// Function Signature:
+
+// function concatenateArrays<T>(...arrays: T[][]): T[]
+// Example:
+
+// concatenateArrays(["a", "b"], ["c"]);       // Output: ["a", "b", "c"]
+// concatenateArrays([1, 2], [3, 4], [5]);     // Output: [1, 2, 3, 4, 5]
+
+function concatenateArrays<T>(...arrays: T[][]): T[]{
+   return arrays.flat()
+}
+// console.log(concatenateArrays(["a", "b"], ["c"]));       
+// console.log(concatenateArrays([1, 2], [3, 4], [5]))    
+
+// ===================================PROBLEM 4===========================
+
+// Problem 4:
+// Description:
+
+// Create a Vehicle class with private make and year properties and a getInfo() method.
+// Create a Car class extending Vehicle, adding a private model property and a getModel() method.
+// Example:
+
+// const myCar = new Car("Toyota", 2020, "Corolla");
+// myCar.getInfo();   // Output: "Make: Toyota, Year: 2020"
+// myCar.getModel();  // Output: "Model: Corolla"
+
+class Vehicle {
+   protected make : string;
+   protected year :number;
+
+   constructor(make:string,year:number){
+      this.make = make;
+      this.year = year;
+   }
+
+   getInfo():string{
+      return `The ${this.make} car was made in ${this.year} year`
+   }
+}
+// const myCar = new Vehicle("Toyota", 2020);
+// console.log(myCar.getInfo())
+
+class Car extends Vehicle {
+   private model:string;
+   constructor(make:string,year:number, model:string){
+      super(make,year);
+      this.model = model
+   }
+   getModel():string{
+      return `The ${this.make} car model numbder ${this.model}`
+   }
+}
+
+
+
+const myCar = new Car("Toyota", 2020,"Corola");
+console.log(myCar.getModel())
